dfe5d352f8867c41bd8d118aff25e98593a2f16a
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-07-17 18:46:14 +0200

    DI 1.0.4

---
+++ src/Rystem.NugetHelper/Rystem.NugetHelper/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string path = @"C:\Users\aless\source\repos";
+List<string> projectNames = new() { "RepositoryFramework", "RystemV3", "Rystem.Concurrency", "Rystem.BackgroundJob", "Rystem.Queue" };
+
+var rystemDirectories = new DirectoryInfo(path).GetDirectories().Where(x => projectNames.Contains(x.Name)).ToList();
+
+foreach (var rystemDirectory in rystemDirectories)
+{
+    
+}

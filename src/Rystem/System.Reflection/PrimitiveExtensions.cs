90c1633881e0a2c448fb0e4da2a47159dba09453
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-07-29 09:27:14 +0200

    v. 4.0.30 primitive

---
+++ src/Rystem/System.Reflection/PrimitiveExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Reflection
+{
+    public static class PrimitiveExtensions
+    {
+        private static readonly Type[] PrimitiveTypes = new Type[] {
+            typeof(string),
+            typeof(decimal),
+            typeof(DateTime),
+            typeof(DateTimeOffset),
+            typeof(TimeSpan),
+            typeof(Guid)
+        };
+        public static bool IsPrimitive<T>(this T entity)
+            => entity?.GetType().IsPrimitive() ?? typeof(T).IsPrimitive();
+        public static bool IsPrimitive(this Type type)
+            => type.IsPrimitive || PrimitiveTypes.Contains(type) || type.IsEnum || Convert.GetTypeCode(type) != TypeCode.Object ||
+            (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) && IsPrimitive(type.GetGenericArguments()[0]));
+    }
+}

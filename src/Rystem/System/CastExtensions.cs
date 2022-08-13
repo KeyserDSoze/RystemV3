46148c65ffaf4c9505027def0fbc14bbe8a79bea
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-08-13 17:24:31 +0200

    v. 4.0.59 cast extensions improved with null check

--- src/Rystem/System/CastExtensions.cs
+++ src/Rystem/System/CastExtensions.cs
@@ -2,9 +2,13 @@
 {
     public static class CastExtensions
     {
-        public static T Cast<T>(this object entity)
+        public static T? Cast<T>(this object? entity)
         {
-            if (entity is IConvertible)
+            if (entity == null)
+                return default;
+            if (entity is T casted)
+                return casted;
+            else if (entity is IConvertible)
                 return (T)Convert.ChangeType(entity, typeof(T));
             else
                 return (T)entity;

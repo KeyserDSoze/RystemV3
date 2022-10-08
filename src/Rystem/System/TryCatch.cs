37fb0118776457853817f1385d665db96815b014
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-10-08 12:13:47 +0200

    v. 4.0.77 - try catch improved

--- src/Rystem/System/TryCatch.cs
+++ src/Rystem/System/TryCatch.cs
@@ -1,51 +1,51 @@
 ﻿namespace System
 {
-    public static class Try
+    public static partial class Try
     {
-        public static T? WithDefaultOnCatch<T>(Func<T> function)
+        public static TryResponse<T> WithDefaultOnCatch<T>(Func<T> function)
         {
             try
             {
-                return function.Invoke();
+                return new(function.Invoke());
             }
-            catch
+            catch (Exception ex)
             {
-                return default;
+                return new(default, ex);
             }
         }
-        [Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S2486:Generic exceptions should not be ignored", Justification = "To skip the exception.")]
-        [Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S108:Nested blocks of code should not be left empty", Justification = "For empty catch.")]
-        public static void WithDefaultOnCatch(Action function)
+        public static Exception? WithDefaultOnCatch(Action function)
         {
             try
             {
                 function.Invoke();
+                return default;
             }
-            catch
+            catch (Exception ex)
             {
+                return ex;
             }
         }
-        public static async Task<T?> WithDefaultOnCatchAsync<T>(Func<Task<T>> function)
+        public static async Task<TryResponse<T>> WithDefaultOnCatchAsync<T>(Func<Task<T>> function)
         {
             try
             {
-                return await function.Invoke();
+                return new(await function.Invoke());
             }
-            catch
+            catch (Exception ex)
             {
-                return default;
+                return new(default, ex);
             }
         }
-        [Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S2486:Generic exceptions should not be ignored", Justification = "To skip the exception.")]
-        [Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S108:Nested blocks of code should not be left empty", Justification = "For empty catch.")]
-        public static async Task WithDefaultOnCatchAsync(Func<Task> function)
+        public static async Task<Exception?> WithDefaultOnCatchAsync(Func<Task> function)
         {
             try
             {
                 await function.Invoke();
+                return default;
             }
-            catch
+            catch (Exception ex)
             {
+                return ex;
             }
         }
     }
---
+++ src/Rystem/System/TryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace System
+{
+    public sealed record TryResponse<T>(T? Entity, Exception? Exception = null)
+    {
+        public static implicit operator T(TryResponse<T> response)
+            => response.Entity!;
+    }
+}

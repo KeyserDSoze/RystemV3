2b15c53fd3fd1dd50e3f0c59fd07e4c61f62f381
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-10-17 18:37:05 +0200

    x

--- src/Rystem.NugetHelper/Rystem.NugetHelper/Engine/Update.cs
+++ src/Rystem.NugetHelper/Rystem.NugetHelper/Engine/Update.cs
@@ -27,7 +27,8 @@
             "Rystem.RepositoryFramework.Infrastructure.InMemory", "Rystem.RepositoryFramework.MigrationTools",
             "Rystem.RepositoryFramework.Cache", "Rystem.RepositoryFramework.Infrastructure.Azure.Cosmos.Sql",
             "Rystem.RepositoryFramework.Infrastructure.Azure.Storage.Blob", "Rystem.RepositoryFramework.Infrastructure.Azure.Storage.Table",
-            "RepositoryFramework.Infrastructure.Dynamics.Dataverse", "RepositoryFramework.Infrastructure.EntityFramework")
+            "RepositoryFramework.Infrastructure.Dynamics.Dataverse", "RepositoryFramework.Infrastructure.EntityFramework",
+            "RepositoryFramework.Infrastructure.MsSql")
             .CreateSon()
             .AddProject("Rystem.BackgroundJob", "Rystem.RepositoryFramework.Cache.Azure.Storage.Blob")
             .CreateSon()

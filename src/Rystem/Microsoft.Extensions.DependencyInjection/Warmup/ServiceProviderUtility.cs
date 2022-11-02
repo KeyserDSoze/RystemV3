3833b0d07a8d470ae6cd864ee8309967e1b6a898
alessandro rapiti <alessandro.rapiti44@gmail.com>
2022-11-02 03:19:17 +0100

    new version v.4.0.84

--- src/Rystem/Microsoft.Extensions.DependencyInjection/Warmup/ServiceProviderUtility.cs
+++ src/Rystem/Microsoft.Extensions.DependencyInjection/Warmup/ServiceProviderUtility.cs
@@ -4,17 +4,18 @@ namespace Rystem
 {
     internal sealed class ServiceProviderUtility
     {
-        public static ServiceProviderUtility Instance { get; set;} = new();
+        public static ServiceProviderUtility Instance { get; set; } = new();
         private ServiceProviderUtility() { }
         public Func<IServiceProvider, Task>? AfterBuildEvent { get; set; }
         public async Task AfterBuildAsync(IServiceProvider providers)
         {
-            var scope = providers.CreateAsyncScope();
-            List<Task> tasks = new();
+            var scope = providers.CreateScope();
             if (AfterBuildEvent != null)
-                tasks.Add(AfterBuildEvent.Invoke(scope.ServiceProvider));
-            await Task.WhenAll(tasks);
-            await scope.DisposeAsync();
+                _ = await Try.WithDefaultOnCatchAsync(() =>
+                {
+                    return AfterBuildEvent.Invoke(scope.ServiceProvider);
+                });
+            scope.Dispose();
         }
     }
 }

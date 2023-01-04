34d602b331c011f04e9cb555e7688bf063b3282e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-01-04 11:44:31 +0100

    csv engine removed all warnings

--- src/Rystem/System.Text.Csv/CsvEngine.cs
+++ src/Rystem/System.Text.Csv/CsvEngine.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace System.Text.Csv
 {
     internal sealed class CsvEngine
     {
-        private sealed class TableHandler
+        private sealed class MapHandler
         {
-            public Dictionary<string, RowHandler> Map { get; set; }
-            public void Add(int valueIndex, BasePropertyNameValue basePropertyNameValue)
+            public required Dictionary<string, RowHandler> Map { get; init; }
+            public void Add(int index, BasePropertyNameValue basePropertyNameValue)
             {
                 if (!Map.ContainsKey(basePropertyNameValue.NavigationPath))
                     Map.Add(basePropertyNameValue.NavigationPath, new RowHandler
@@ -20,33 +18,31 @@ namespace System.Text.Csv
                         Rows = new()
                     });
                 var map = Map[basePropertyNameValue.NavigationPath];
-                if (map.Rows.Count <= valueIndex)
+                if (map.Rows.Count <= index)
                     map.Rows.Add(new() { Columns = new() });
-                var row = map.Rows[valueIndex];
-                if (!row.Columns.ContainsKey(basePropertyNameValue.Name))
-                    row.Columns.Add(basePropertyNameValue.Name, new());
-                var listOfValues = row.Columns[basePropertyNameValue.Name];
+                var row = map.Rows[index];
+                if (!row.Columns.ContainsKey(basePropertyNameValue.Name!))
+                    row.Columns.Add(basePropertyNameValue.Name!, new());
+                var listOfValues = row.Columns[basePropertyNameValue.Name!];
                 listOfValues.Add(basePropertyNameValue.Value?.ToString() ?? string.Empty);
                 if (listOfValues.Count > map.Max)
                     map.Max = listOfValues.Count;
             }
-        }
-        private static readonly Regex Regex = new(@"\[[^\]]*\]");
-        private sealed class ColumnHandler
-        {
-            public Dictionary<string, List<string>> Columns { get; set; }
         }
         private sealed class RowHandler
         {
             public required string NavigationPath { get; init; }
             public required List<ColumnHandler> Rows { get; init; }
             public required int Max { get; set; }
-
+        }
+        private sealed class ColumnHandler
+        {
+            public required Dictionary<string, List<string>> Columns { get; init; }
         }
         public static string Convert<T>(IEnumerable<T> values)
         {
             var showcase = typeof(T).ToShowcase();
-            var tableHandler = new TableHandler() { Map = new() };
+            var tableHandler = new MapHandler() { Map = new() };
             int counter = 0;
             foreach (var value in values)
             {
@@ -85,41 +81,40 @@ namespace System.Text.Csv
                     }
                 }
             }
-            StringBuilder header = new();
-            foreach (var map in tableHandler.Map)
+
+            var header = new StringBuilder();
+            foreach (var map in tableHandler.Map.Select(x => x.Value))
             {
-                if (map.Value.Max < 2)
+                if (map.Max < 2)
                 {
-                    foreach (var value in map.Value.Rows.First().Columns)
-                        if (string.IsNullOrWhiteSpace(map.Value.NavigationPath))
-                            header.Append($"{value.Key},");
+                    foreach (var key in map.Rows.First().Columns.Select(x => x.Key))
+                        if (string.IsNullOrWhiteSpace(map.NavigationPath))
+                            header.Append($"{key},");
                         else
-                            header.Append($"{map.Value.NavigationPath}.{value.Key},");
+                            header.Append($"{map.NavigationPath}.{key},");
                 }
                 else
                 {
-                    foreach (var value in map.Value.Rows.First().Columns)
-                        for (int i = 0; i < map.Value.Max; i++)
-                        {
-                            header.Append($"{map.Value.NavigationPath}[{i}].{value.Key},");
-                        }
+                    foreach (var key in map.Rows.First().Columns.Select(x => x.Key))
+                        for (int i = 0; i < map.Max; i++)
+                            header.Append($"{map.NavigationPath}[{i}].{key},");
                 }
             }
-            StringBuilder[] rows = new StringBuilder[counter];
-            foreach (var map in tableHandler.Map)
+            var rows = new StringBuilder[counter];
+            foreach (var map in tableHandler.Map.Select(x => x.Value))
             {
                 int internalCounter = 0;
-                foreach (var row in map.Value.Rows)
+                foreach (var row in map.Rows.Select(x => x.Columns))
                 {
                     if (rows[internalCounter] == null)
                         rows[internalCounter] = new StringBuilder();
                     var stringBuilder = rows[internalCounter];
-                    foreach (var column in row.Columns)
+                    foreach (var columnValues in row.Select(x => x.Value))
                     {
                         if (stringBuilder.Length > 0)
                             stringBuilder.Append(',');
-                        stringBuilder.Append(string.Join(',', column.Value.Select(x => CheckIfContainsEscapeCharacters(x))));
-                        for (int i = column.Value.Count; i < map.Value.Max; i++)
+                        stringBuilder.Append(string.Join(',', columnValues.Select(x => CheckIfContainsEscapeCharacters(x))));
+                        for (int i = columnValues.Count; i < map.Max; i++)
                             stringBuilder.Append(',');
                     }
                     internalCounter++;
@@ -128,6 +123,7 @@ namespace System.Text.Csv
 
             string CheckIfContainsEscapeCharacters(string value)
                 => value.Contains(',') || value.Contains('"') ? $"\"{value}\"" : value;
+
             return $"{header.ToString().Trim(',')}{'\n'}{string.Join('\n', rows.Select(x => x.ToString()))}";
         }
     }
